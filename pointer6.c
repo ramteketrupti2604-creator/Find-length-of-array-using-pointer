7a9214df0613a335ed4039153810793a2614958a
ramteketrupti2604-creator <ramteketrupti2604@gmail.com>
2026-01-13 10:17:50 +0530

    Add files via upload

---
+++ pointer6.c
@@ -0,0 +1,15 @@
+//Find length of array using pointer
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int *p = arr;
+
+    for(int i = 0; i < size; i++) {
+        printf("%d ", *(p + i));
+    }
+
+    return 0;
+}
